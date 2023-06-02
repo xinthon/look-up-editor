c6aa28dcfa77bd113dcb302f133ebc18b9a550aa
Sinthon Seng <s.sinthon@cas.com.kh>
2023-06-02 11:27:57 +0700

    add add more style

--- look-up-editor/Product.cs
+++ look-up-editor/Product.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,14 @@ namespace look_up_editor
     public class Product
     {
         public int ID { get; set; }
-        public string ProductName { get; set; } = string.Empty; 
+        public string ProductName { get; set; } = string.Empty;
+        [Hidden]
         public string Country { get; set; } = string.Empty;
+        [Hidden]
         public string City { get; set; } = string.Empty;
+        [Hidden]
         public double UnitPrice { get; set; }
+        [Hidden]
         public int Quantity { get; set; }
     }
 }
